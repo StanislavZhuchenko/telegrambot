08f36d2ca6003bb2b6bb22f7026ca95113a11dc0
Stanislav Zhuchenko <Zhuchenko_Stas@ukr.net>
2023-08-08 21:52:32 +0300

    'update parser and bot'

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import asyncio
 import logging
-import json
-from random import randint
+import datetime
 
 from test import get_gp_results, dict_of_round_name
 
@@ -11,8 +10,10 @@ from aiogram import F
 
 from secret import token
 
-from aiogram.enums.dice_emoji import DiceEmoji
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from parser import find_all_races_of_year, current_race_results, formatted_summary_of_year, summary_results_of_year, \
+    formatted_current_race_results
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -20,67 +21,131 @@ bot = Bot(token=token)
 dp = Dispatcher()
 
 
-# with open('f1_2023.json', 'r') as f:
-#     current_results = json.load(f)
-
-
-@dp.message(Command("start"))
+@dp.message(Command('start'))
 async def cmd_start(message: types.Message):
     kb = [
         [
-            types.KeyboardButton(text="/GP"),
+            types.KeyboardButton(text='/Results'),
+            types.KeyboardButton(text='/Drivers'),
+            types.KeyboardButton(text='/Teams'),
+            types.KeyboardButton(text='/Schedule'),
         ],
     ]
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=kb,
         resize_keyboard=True,
     )
-    await message.answer(text=f"Hello, {message.from_user.full_name}", reply_markup=keyboard)
+    await message.answer(text=f'Hello, {message.from_user.full_name}', reply_markup=keyboard)
 
 
-
-
-@dp.message(Command("GP"))
-async def all_grandprix(message: types.Message):
+@dp.message(Command('Results'))
+async def results(message: types.Message):
     builder = InlineKeyboardBuilder()
-    for element in dict_of_round_name:
-        builder.add(types.InlineKeyboardButton(
-            text=f"{dict_of_round_name[element]}",
-            callback_data=f"{element}")
-        )
-    builder.adjust(3)
+    builder.add(types.InlineKeyboardButton(
+        text=str(datetime.date.today().year),
+        callback_data='Current_year_results'
+    ))
+    builder.add(types.InlineKeyboardButton(
+        text=f'Archive 1950-{datetime.date.today().year - 1}',
+        callback_data='Archive_results'
+    ))
+    builder.adjust(2)
+    await message.answer('Make a choice', reply_markup=builder.as_markup())
 
-    await message.answer(
-        "Choice GP",
+
+@dp.callback_query(F.data == 'Current_year_results')
+async def current_year_results(callback: types.CallbackQuery, year=str(datetime.date.today().year)):
+    all_done_races_of_year = summary_results_of_year(year)
+    # year = str(datetime.date.today().year)
+    builder = InlineKeyboardBuilder()
+    for race in all_done_races_of_year:
+        builder.add(types.InlineKeyboardButton(
+            text=race,
+            callback_data=f'{race}, {year}'
+        ))
+
+    builder.adjust(2)
+
+    results = formatted_summary_of_year(all_done_races_of_year)
+    await callback.message.answer(
+        results,
         reply_markup=builder.as_markup()
     )
 
 
-@dp.callback_query(F.data.in_(dict_of_round_name))
-async def current_grandprix(callback: types.CallbackQuery):
-    text_for_button = (
-        'Race result', 'Qualifying',
-        'Fastest lap', 'Practice 3',
-        'Pit stop summary', 'Practice 2',
-        'Starting grid', 'Practice 1'
-    )
+@dp.callback_query(F.data == 'Archive_results')
+async def choice_a_year(callback: types.CallbackQuery):
+    await callback.message.answer(f'Enter year between 1950 and {datetime.date.today().year - 1}')
 
+
+@dp.message(F.text.regexp(r'[1|2][9|0][\d]{2}$'))
+async def archive_results(message: types.Message):
+    year = message.text
+    all_done_races_of_year = summary_results_of_year(year)
+    # year = str(datetime.date.today().year)
     builder = InlineKeyboardBuilder()
-    for text in text_for_button:
+    for race in all_done_races_of_year:
         builder.add(types.InlineKeyboardButton(
-            text=f"{text}",
-            callback_data=f"{text}, {callback.data}"
+            text=race,
+            callback_data=f'{race}, {year}'
         ))
     builder.adjust(2)
+    results = formatted_summary_of_year(all_done_races_of_year)
+    await message.answer(
+        results,
+        reply_markup=builder.as_markup()
+    )
 
-    await callback.message.answer(str(dict_of_round_name[callback.data]), reply_markup=builder.as_markup())
+
+@dp.callback_query(F.data.regexp(r'\D+\,\s\d{4}$'))
+async def current_gp_result(callback: types.CallbackQuery):
+    d = callback.data.split(',')
+    result = formatted_current_race_results(current_race_results(d[0], int(d[1])))
+    await callback.message.answer(str(result))
 
 
-@dp.callback_query(F.data.contains('Race result'))
-async def result_of_grandprix(callback: types.CallbackQuery):
-    number_of_gp = int(callback.data.split(',')[1])
-    await callback.message.answer(get_gp_results(number_of_gp))
-
+#
+# @dp.message(Command("GP"))
+# async def all_grandprix(message: types.Message):
+#     builder = InlineKeyboardBuilder()
+#     for element in dict_of_round_name:
+#         builder.add(types.InlineKeyboardButton(
+#             text=f"{dict_of_round_name[element]}",
+#             callback_data=f"{element}")
+#         )
+#     builder.adjust(3)
+#
+#     await message.answer(
+#         "Choice GP",
+#         reply_markup=builder.as_markup()
+#     )
+#
+#
+# @dp.callback_query(F.data.in_(dict_of_round_name))
+# async def current_grandprix(callback: types.CallbackQuery):
+#     text_for_button = (
+#         'Race result', 'Qualifying',
+#         'Fastest lap', 'Practice 3',
+#         'Pit stop summary', 'Practice 2',
+#         'Starting grid', 'Practice 1'
+#     )
+#
+#     builder = InlineKeyboardBuilder()
+#     for text in text_for_button:
+#         builder.add(types.InlineKeyboardButton(
+#             text=f"{text}",
+#             callback_data=f"{text}, {callback.data}"
+#         ))
+#     builder.adjust(2)
+#
+#     await callback.message.answer(str(dict_of_round_name[callback.data]), reply_markup=builder.as_markup())
+#
+#
+# @dp.callback_query(F.data.contains('Race result'))
+# async def result_of_grandprix(callback: types.CallbackQuery):
+#     number_of_gp = int(callback.data.split(',')[1])
+#     await callback.message.answer(get_gp_results(number_of_gp))
+#
 
 
 async def main():
--- parser.py
+++ parser.py
@@ -1,36 +1,119 @@
 import requests
 import logging
 from bs4 import BeautifulSoup
+import datetime
 
 logging.basicConfig(level=logging.DEBUG)
 
-r = requests.get('https://www.formula1.com/en/results.html/2023/races/1141/bahrain/race-result.html')
-soup = BeautifulSoup(r.text, 'lxml')
-results = soup.find('div', {'class': 'resultsarchive-wrapper'}).prettify()
 
-table_of_results = soup.find('table', {'class': 'resultsarchive-table'})
+def find_all_races_of_year(year=datetime.date.today().year):
+    current_year = datetime.date.today().year
+    if 1950 <= int(year) <= current_year:
+        response = requests.get(f'https://www.formula1.com/en/results.html/{year}/races.html')  # Link to all races
+        soup = BeautifulSoup(response.text, 'lxml')
+        div_with_results = soup.select('div.resultsarchive-filter-wrap:nth-of-type(3)')
+        all_races = dict()
+        for element in div_with_results:
+            a_with_results = element.select('a')
+            for a_attrs in a_with_results:
+                all_races[a_attrs.text.strip()] = 'https://www.formula1.com' + a_attrs.attrs['href']
+        return all_races
+    else:
+        return None
 
-dict_of_res = dict()
+# print('find_all_races_of_year', find_all_races_of_year())
+
+def summary_results_of_year(year=datetime.date.today().year):
+
+    response = requests.get(f'https://www.formula1.com/en/results.html/{year}/races.html')
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    table_results = soup.select_one('.resultsarchive-table').select_one('tbody').find_all('tr')
+    race_results = dict()
+    for row in table_results:
+        res = row.text.split('\n')
+        res1 = [el for el in res if len(el) != 0 and not el.isspace()]
+        race, date, name, surname, abbr, constructor, laps, full_time = [data.strip() for data in res1]
+        row_res = {'date': date, 'name': name, 'surname': surname, 'abbr': abbr,
+                   'constructor': constructor, 'laps': laps, 'full_time': full_time}
+        race_results[race] = row_res
+    return race_results
 
 
-for row in table_of_results.find_all('tr'):
-    driver = []
-    for data in row.find_all('td'):
-        if not data.text.isspace() and not 'limiter' in data.attrs['class']:
-            driver.append(data.text.strip())
+def formatted_summary_of_year(results):
+    string_of_data = 'GRAND PRIX, WINNER, CAR\n'
+    for data in results:
+        string_of_data += data + ' ' + results[data]['name'] + ' ' + results[data]['abbr'] + ' ' + results[data]['constructor']+'\n'
+    return string_of_data
 
-    if driver:
-        position, number, name, constructor, laps, time, points = [el.replace('\n', ' ') for el in driver]
-        row_data = {
-            'number': position,
-            'name': name,
-            'constructor': constructor,
-            'laps': laps,
-            'time': time,
-            'points': points,
-        }
+# print('summary_results_of_year', summary_results_of_year(2012))
+# print(formatted_summary_of_year(2023))
 
-        dict_of_res[position] = row_data
+# FIND LINK TO SELECTED GP
+# response = requests.get('https://www.formula1.com/en/results.html/2023/races.html')  # Link to all races
+# soup = BeautifulSoup(response.text, 'lxml')
+# div_with_results = soup.select('div.resultsarchive-filter-wrap:nth-of-type(3)')
+# for element in div_with_results:
+#     a_with_results = element.select('a')
+#     for a_attrs in a_with_results:
+#         print(a_with_results)
+#         # print(row.text.strip()) # All Name of GP
+#         if a_attrs.text.strip() == 'Qatar':  # remove all spaces
+#             links_to_race_results = a_attrs.attrs['href']  # access to href attrs in a_with_results
+#
+# full_link = 'https://www.formula1.com' + links_to_race_results
+#
+# print(full_link)
+#
+
+def current_race_results(race, year):
+    full_link = find_all_races_of_year(year)[race]
+
+    r = requests.get(full_link)
+    soup = BeautifulSoup(r.text, 'lxml')
+    results = soup.find('div', {'class': 'resultsarchive-wrapper'}).prettify()
+
+    table_of_results = soup.find('table', {'class': 'resultsarchive-table'})
+
+    if not table_of_results:
+        print('No results are currently available')
 
 
-print(dict_of_res)
+    dict_of_res = dict()
+
+    count = 1
+    for a_attrs in table_of_results.find_all('tr'):
+        driver = []
+
+        for data in a_attrs.find_all('td'):
+            if not data.text.isspace() and not 'limiter' in data.attrs['class']:
+                driver.append(data.text.strip())
+        # print(driver)
+        if driver:
+            position, number, fullname, constructor, laps, time, points = [el for el in driver]
+            abbr = fullname.split('\n')[-1]
+            name = ' '.join(fullname.split('\n')[0:2])
+            row_data = {
+                'position': position,
+                'number': number,
+                'name': name,
+                'abbr': abbr,
+                'constructor': constructor,
+                'laps': laps,
+                'time': time,
+                'points': points,
+            }
+
+            dict_of_res[count] = row_data
+            count += 1
+    return dict_of_res
+
+def formatted_current_race_results(results):
+    string_of_data = 'POS, NAME, CAR\n'
+    for data in results:
+        string_of_data += f"{results[data]['position']} {results[data]['name']} {results[data]['constructor']}\n"
+    return string_of_data
+
+# print(current_race_results('Bahrain', 2023))
+# print(formatted_current_race_results(current_race_results('Bahrain', 2023)))
+
